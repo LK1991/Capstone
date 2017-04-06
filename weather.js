32d1ae00c804a20522c520f7a1070706bce009c9
Lianna Katrdzhyan <lkatrdzhyan@gmail.com>
2017-04-06 09:16:14 -0700

    Still trying to display an image with js

--- weather.js
+++ weather.js
@@ -11,8 +11,8 @@ $(function() {
       // Our Data
       var icon = data.weather[0].icon;
       var icon_replace = $(".weather-app_main__information--icon").attr("src");
-      $(".weather-app_main__information--city").html(data.name);
       var temp = Math.round(data.main.temp * 9 / 5 + 32);
+      $(".weather-app_main__information--city").html(data.name);
       var description = data.weather[0].description;
       var cDescription = description.charAt(0).toUpperCase() + description.slice(1);
 
@@ -22,82 +22,91 @@ $(function() {
       $(".weather-app_main__information--description").html(cDescription);
     })
   })
+  function getOutfit() {
+    var htmlstr = '';
+    htmlstr.setAttribute("width", "304");
+    htmlstr.setAttribute("height", "228");
+    if(temp == 59) {
+      htmlstr += '<img src = "images/1stoutfit.png"';
+    } else {
+      return "hi";
+    }
+     return htmlstr;
+  }
+
+  document.getElementById("weatherNow").innerHTML = getOutfit();
 });
 
 var imgArray = new Array();
 
 imgArray[0] = new Image();
-imgArray[0] = "images/1stoutfit.png";
+imgArray[0].src = "images/1stoutfit.png";
 
 imgArray[1] = new Image();
-imgArray[1] = "images/1outfitt.png";
+imgArray[1].src = "images/1outfitt.png";
 
 imgArray[2] = new Image();
-imgArray[2] = "images/2ndoutfit.png";
+imgArray[2].src = "images/2ndoutfit.png";
 
 imgArray[3] = new Image();
-imgArray[3] = "images/2ndoutfitt.png";
+imgArray[3].src = "images/2ndoutfitt.png";
 
 imgArray[4] = new Image();
-imgArray[4] = "images/3rdoutfit.png";
+imgArray[4].src = "images/3rdoutfit.png";
 
 imgArray[5] = new Image();
-imgArray[5] = "images/3rdoutfitt.png";
+imgArray[5].src = "images/3rdoutfitt.png";
 
 imgArray[6] = new Image();
-imgArray[6] = "images/4thoutfit.png";
+imgArray[6].src = "images/4thoutfit.png";
 
 imgArray[7] = new Image();
-imgArray[7] = "images/4thoutfitt.png";
+imgArray[7].src = "images/4thoutfitt.png";
 
 imgArray[8] = new Image();
-imgArray[8] = "images/5thoutfit.png";
+imgArray[8].src = "images/5thoutfit.png";
 
 imgArray[9] = new Image();
-imgArray[9] = "images/5thoutfitt.png";
+imgArray[9].src = "images/5thoutfitt.png";
 
 imgArray[10] = new Image();
-imgArray[10] = "images/6thoutfit.png";
+imgArray[10].src = "images/6thoutfit.png";
 
 imgArray[11] = new Image();
-imgArray[11] = "images/6thoutfitt.png";
+imgArray[11].src = "images/6thoutfitt.png";
 
 imgArray[12] = new Image();
-imgArray[12] = "images/guyoutfit1.png";
+imgArray[12].src = "images/guyoutfit1.png";
 
 imgArray[13] = new Image();
-imgArray[13] = "images/guyoutfit2.png";
+imgArray[13].src = "images/guyoutfit2.png";
 
 imgArray[14] = new Image();
-imgArray[14] = "images/guyoutfit3.png";
+imgArray[14].src = "images/guyoutfit3.png";
 
 imgArray[15] = new Image();
-imgArray[15] = "images/guyoutfit4.png";
+imgArray[15].src = "images/guyoutfit4.png";
 
 imgArray[16] = new Image();
-imgArray[16] = "images/guyoutfit5.png";
+imgArray[16].src = "images/guyoutfit5.png";
 
 imgArray[17] = new Image();
-imgArray[17] = "images/guyoutfit6.png";
+imgArray[17].src = "images/guyoutfit6.png";
 
 imgArray[18] = new Image();
-imgArray[18] = "images/guyoutfit22.png";
+imgArray[18].src = "images/guyoutfit22.png";
 
 imgArray[19] = new Image();
-imgArray[19] = "images/guyoutfit33.png";
+imgArray[19].src = "images/guyoutfit33.png";
 
 imgArray[20] = new Image();
-imgArray[20] = "images/guyoutfit44.png";
+imgArray[20].src = "images/guyoutfit44.png";
 
 imgArray[21] = new Image();
-imgArray[21] = "images/guyoutfit55.png";
+imgArray[21].src = "images/guyoutfit55.png";
 
 imgArray[22] = new Image();
-imgArray[22] = "images/guyoutfit66.png";
+imgArray[22].src = "images/guyoutfit66.png";
 
 imgArray[23] = new Image();
-imgArray[23] = "images/guyoutfitt.png";
-
-function chooseOutfit() {
-
-}
+imgArray[23].src = "images/guyoutfitt.png";
