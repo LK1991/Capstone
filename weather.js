134d750cc004c743cdccc6b77ace8b14401888e0
Lianna Katrdzhyan <lkatrdzhyan@gmail.com>
2017-04-11 08:30:58 -0700

    sdfsd

--- weather.js
+++ weather.js
@@ -1,4 +1,4 @@
-var ourTemp=0;
+var ourTemp = 0;
 
 $(function() {
 
@@ -27,87 +27,10 @@ $(function() {
   })
 
   function getOutfit() {
-    var htmlstr = '';
-    if(ourTemp >= 60) {
+    if (ourTemp <= 60) {
       document.getElementById("weatherNow").innerHTML = '<img width=300 src= "images/1stoutfit.png" >';
-    } else () {
-
-    }
+    } 
   }
-
-  var imgArray = new Array();
-
-  imgArray[0] = new Image();
-  imgArray[0].src = "images/1stoutfit.png";
-
-  imgArray[1] = new Image();
-  imgArray[1].src = "images/1stoutfitt.png";
-
-  imgArray[2] = new Image();
-  imgArray[2].src = "images/2ndoutfit.png";
-
-  imgArray[3] = new Image();
-  imgArray[3].src = "images/2ndoutfitt.png";
-
-  imgArray[4] = new Image();
-  imgArray[4].src = "images/3rdoutfit.png";
-
-  imgArray[5] = new Image();
-  imgArray[5].src = "images/3rdoutfitt.png";
-
-  imgArray[6] = new Image();
-  imgArray[6].src = "images/4thoutfit.png";
-
-  imgArray[7] = new Image();
-  imgArray[7].src = "images/4thoutfitt.png";
-
-  imgArray[8] = new Image();
-  imgArray[8].src = "images/5thoutfit.png";
-
-  imgArray[9] = new Image();
-  imgArray[9].src = "images/5thoutfitt.png";
-
-  imgArray[10] = new Image();
-  imgArray[10].src = "images/6thoutfit.png";
-
-  imgArray[11] = new Image();
-  imgArray[11].src = "images/6thoutfitt.png";
-
-  imgArray[12] = new Image();
-  imgArray[12].src = "images/guyoutfit1.png";
-
-  imgArray[13] = new Image();
-  imgArray[13].src = "images/guyoutfit2.png";
-
-  imgArray[14] = new Image();
-  imgArray[14].src = "images/guyoutfit3.png";
-
-  imgArray[15] = new Image();
-  imgArray[15].src = "images/guyoutfit4.png";
-
-  imgArray[16] = new Image();
-  imgArray[16].src = "images/guyoutfit5.png";
-
-  imgArray[17] = new Image();
-  imgArray[17].src = "images/guyoutfit6.png";
-
-  imgArray[18] = new Image();
-  imgArray[18].src = "images/guyoutfit22.jpg";
-
-  imgArray[19] = new Image();
-  imgArray[19].src = "images/guyoutfit33.jpg";
-
-  imgArray[20] = new Image();
-  imgArray[20].src = "images/guyoutfit44.png";
-
-  imgArray[21] = new Image();
-  imgArray[21].src = "images/guyoutfit55.jpg";
-
-  imgArray[22] = new Image();
-  imgArray[22].src = "images/guyoutfit66.jpg";
-
-  imgArray[23] = new Image();
-  imgArray[23].src = "images/guyoutfitt.jpg";
 
 getOutfit();
 
