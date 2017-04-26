c647ff862b7a1db088ae4173d72fcc7d502a07d2
Lianna Katrdzhyan <lkatrdzhyan@gmail.com>
2017-04-26 08:44:24 -0700

    Personal Closet Demo

--- login.php
+++ login.php
@@ -68,8 +68,14 @@
         <input type="password" id="pass" name="pass" />
       </p>
       <p>
-        <button onclick="clickFunction()" class="w3-button w3-hover-teal">Login </button>
+        <button onclick="clickFunction()" class="w3-button w3-hover-teal" >Login </button>
       </p>
+      <script type="text/javascript">
+        function clickFunction() {
+          alert("You're Logged In!");
+          $("button").setAttribute("href", "home.html");
+        }
+      </script>
      </form>
   </div>
   <div id = "personalCloset">
